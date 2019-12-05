c33ee8b23df9493242da0b9e96d23a5e49aa0e18
jasmeet kaur <jasmeet.bjf@gmail.com>
2019-12-05 15:32:30 -0600

    add babel

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
+const babel = require('gulp-babel');
 
 function styles() {
   return gulp.src('src/css/**/*.css')
@@ -16,5 +17,11 @@ function images() {
   .pipe(gulp.dest('dest/images'))
 }
 
+function babel() {
+  return gulp.src('./src/js/**/*.js')
+  .pipe(babel({
+    presets: ['@babel/env']
+  }))
+}
 exports.styles = styles;
 exports.images = images;
