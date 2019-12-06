e4118f68e7f492e029bd37dbd91961dce8a3f102
jasmeet kaur <jasmeet.bjf@gmail.com>
2019-12-05 21:40:01 -0600

    add uglify

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
 const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
 function styles() {
   return gulp.src('src/css/**/*.css')
@@ -17,11 +19,15 @@ function images() {
   .pipe(gulp.dest('dest/images'))
 }
 
-function babel() {
+function jsfile() {
   return gulp.src('./src/js/**/*.js')
   .pipe(babel({
     presets: ['@babel/env']
   }))
+  .pipe(concat('main.js'))
+  .pipe(uglify('all.min.js'))
+  .pipe(gulp.dest('./dest/js'))
 }
 exports.styles = styles;
 exports.images = images;
+exports.jsfile = jsfile;
